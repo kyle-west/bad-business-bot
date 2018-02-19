21b12fc0bea54cbc971b4d43ccf2b642238cf21a
Kyle West <wes12009@byui.edu>
2018-02-19 12:41:53 -0700

    Added a database of companies and nouns. It is acutally kind of funny now

---
+++ index.js
@@ -0,0 +1,22 @@
+const db = require('./data.json');
+
+function generateBBB () {
+   var i = Math.floor(Math.random() * db.companies.length);  
+   var j = Math.floor(Math.random() * db.nouns.length); 
+   return getTransition(db.companies[i], db.nouns[j]);
+}
+
+function getTransition (company, noun) {
+   switch(Math.floor(Math.random() * 5)) {
+      case 0: return `It's like ${company} for ${noun}.`;
+      case 1: return `Fancy new start-up idea! It's kind like ${company} for ${noun}.`;
+      case 2: return `Has anyone ever done something like ${company} for ${noun}? I think it might be a good start-up.`;
+      case 3: return `${company} for ${noun}. This could be huge!`;
+      default: return `Guys, I got a great new business idea: ${company} for ${noun}.`;;
+   } 
+}
+
+console.log(generateBBB());
+console.log(generateBBB());
+console.log(generateBBB());
+console.log(generateBBB());
